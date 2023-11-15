e63111a2d1c22ed448ff0949d33bab78a1ae78c5
Pablo Prado <pradopablo502@gmail.com>
2023-11-15 11:20:38 -0600

    Initial commit

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -5,10 +5,29 @@ interface ContainerProps {
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+
+  var q = document.getElementById("q");
+  var qs = document.getElementById("qs");
+  var converted = document.getElementById("converted");
+  q?.addEventListener('change',function(){
+    if (q && converted && qs) {
+      var inputValue = parseFloat((q as HTMLInputElement).value);
+      qs.textContent = inputValue.toString();
+      converted.textContent = (inputValue * 8).toString(); 
+    } else {
+      alert("NULL");
+    }
+  });
+
   return (
     <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <h1>DivisAPP</h1>
+      <p>Convierte dolares a quetzales.</p>
+      <div className="input-group">
+        <label htmlFor="q">Ingresa una cantidad en quetzales:</label>
+        <input type="number" name="q" id="q" placeholder='Q0.00' step='0.01'/>
+      </div>
+      <p className="result">Q<span id='qs'>0.00</span> = $<span id='converted'>0.00</span></p>
     </div>
   );
 };
